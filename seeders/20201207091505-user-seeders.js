78441e23841009f14d300ab51f3390612197e231
imaman15 <imamagustiannugraha@ymail.com>
2021-02-25 12:42:42 +0700

    First init

---
+++ seeders/20201207091505-user-seeders.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const bycrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkInsert('users', [
+      {
+        name: 'Imam Agustian Nugraha',
+        profession: "Admin Micro",
+        role: "admin",
+        email: "imamagustiannugraha@ymail.com",
+        password: await bycrypt.hash('adminimam123', 10),
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Alanna Izzatul Hikmah',
+        profession: "Front End Developer",
+        role: "student",
+        email: "alannaizzatul@gmail.com",
+        password: await bycrypt.hash('studentimam123', 10),
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ]);
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkDelete('users', null, {});
+
+  }
+};
